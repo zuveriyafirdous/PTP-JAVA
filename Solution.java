4c8ecbb1044f29bff510ab60ab79c86022731519
zuveriyafirdous <zuveriyafirdous5@gmail.com>
2025-03-18 20:03:29 +0530

    Add files via upload
    19/03/2025

---
+++ Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Solution{
+	public static void main(String...args)
+	{
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		double d = scan.nextDouble();
+		String str = scan.nextLine();
+
+		System.out.println("myInt is: "+a);
+		System.out.println("myString is: "+str);
+		System.out.println("myDouble is: "+d);
+	}
+}
