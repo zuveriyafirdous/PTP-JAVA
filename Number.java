50453adffedb85327a5865dcb1b8372b835ead55
zuveriyafirdous <zuveriyafirdous5@gmail.com>
2025-03-24 20:32:53 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Number{
+	public static void main(String...args){
+		Scanner sc = new Scanner(System.in);
+		int x= sc.nextInt();
+		System.out.println((x>0) ? "Number is positive" : "Number is negative");
+		
+	}
+
+}
