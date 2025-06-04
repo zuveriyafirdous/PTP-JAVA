b23ce72fcdac20de8ebeec1a4e35b234f6764fbc
zuveriyafirdous <zuveriyafirdous5@gmail.com>
2025-06-04 21:40:36 +0530

    Add files via upload

---
+++ Smallest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Smallest{
+	public static String smallest(int n1,int n2,int n3){
+		if((n1<n2)&&(n1<n3)){
+			return("The smallest value is "+n1);
+		}
+		else if((n2<n1)&&(n2<n3)){
+			return("The smallest value is "+n2);
+		}
+		else{
+			return("The smallest value is "+n3);
+		}
+	}
+	public static void main(String...args){
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int n3 = sc.nextInt();
+
+		System.out.println(smallest(n1, n2, n3));
+	}
+}
